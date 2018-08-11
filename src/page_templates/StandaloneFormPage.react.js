9d5625031c8e32c149ebf8a62cb25e4dc336cf47
Tetsuro Takemoto <t@ineeza.com>
2018-08-11 22:36:49 +0900

    Remove tabler.svg

--- src/page_templates/StandaloneFormPage.react.js
+++ src/page_templates/StandaloneFormPage.react.js
@@ -14,7 +14,6 @@ function StandaloneFormPage(props: Props): React.Node {
           <div className="row">
             <div className="col col-login mx-auto">
               <div className="text-center mb-6">
-                <img src="./assets/brand/tabler.svg" className="h-6" alt="" />
               </div>
               {props.children}
             </div>
